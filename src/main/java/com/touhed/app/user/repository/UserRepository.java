4d157c5b337e3ffb8fac4ef08fa5ba68344a96a2
preaus007 <touhedcs@gmail.com>
2026-02-03 13:15:46 +0600

    fix: remove unused imports

--- src/main/java/com/touhed/app/user/repository/UserRepository.java
+++ src/main/java/com/touhed/app/user/repository/UserRepository.java
@@ -1,11 +1,7 @@
 package com.touhed.app.user.repository;
 
 import com.touhed.app.user.model.User;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
